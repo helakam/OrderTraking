6cd8ff439f487e13102046eda8748e76a5f7ca7f
Nimantha815 <56678166+Nimantha815@users.noreply.github.com>
2021-03-26 23:53:36 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
